aa640ee69a2a683c1d6668ae0a0302c91066c328
Shreyas4895 <jshreyas11@gmail.com>
2022-10-06 09:26:59 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
 
 function App() {
+
+  const [endpoint,setEndpoint]=useState('')
+  const [container,setContainer]=useState([])
+  const [finalpoint,setFinalpoint]=useState('')
+
+
+  useEffect(()=>{
+    fetchMe()
+  },[finalpoint])
+
+const fetchMe =()=> {
+
+  fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endpoint}`, {
+    "method": 'GET',
+    "headers": {
+      'X-RapidAPI-Key': 'a7976963admsh3ba4f9eed628e2ap12f935jsn53443e42349b',
+      'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+    }
+  })
+    .then(response =>{
+      return response.json()
+    })
+    .then(data=>{
+    setContainer(data.d)})
+
+    .catch(err =>{
+      console.error(err)
+    });
+}
+
+const onChangeHandler=(e)=>{
+setEndpoint(e.target.value)
+}
+
+
+const submitHandler=(e)=>{
+  e.preventDefault()
+  setFinalpoint(endpoint);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={submitHandler}>
+      <input type="text" value={endpoint}  onChange={onChangeHandler}/>
+      <button type="submit">submit</button>
+      </form>
+      <div className="element">
+      {container.map((item,index)=>{
+        return(
+        <div key={index} className='element-div'>
+          <img src={item.i.imageUrl} alt=""/>
+          <p>{item.l}</p>
+          </div>
+      )
+      })}
+      </div>
     </div>
   );
 }
